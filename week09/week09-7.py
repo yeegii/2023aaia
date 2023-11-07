917664345ca16133a3fb160b777f833b12bff267
yeegii <yeegii@gmail.com>
2023-11-07 12:09:35 +0800

    week09

--- week09/week09-7.py
+++ week09/week09-7.py
@@ -4,7 +4,11 @@ class Solution:
         N=len(a)
 
 
-       
+        for k in range(N):
+            for i in range(N-1):
+                if a[i+1] < a[i]:
+                    a[i], a[i+1] = a[i+1], a[i]
+  
         for i in range(N-1):
             d= a[1] - a[0] #基礎的距離
             if a[i+1] - a[i] !=d: #距離不相等
