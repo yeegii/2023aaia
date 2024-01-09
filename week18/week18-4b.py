972139cc30f4d09e8a89224c6ec987467a6383aa
yeegii <yeegii@gmail.com>
2024-01-09 12:01:30 +0800

    week18

---
+++ week18/week18-4b.py
@@ -0,0 +1,11 @@
+#字串
+a = 'Hello'
+b = 'world'
+print( a+b )
+print( a*3 )
+print( a[0], a[-1] )
+a = list(map(int, '10 20 30'.split()))
+print(a)
+a = ['Hello', 'world', 'very', 'good']
+print( a[0]+a[1] )
+print('###'.join(a))
